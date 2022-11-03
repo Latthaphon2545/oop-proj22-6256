598902ee579772d6659bc3ec5a508ee232b9e7b0
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2022-11-03 19:24:45 +0700

    .

---
+++ Assets/Scenes/LoginandRegister/Register.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Register : MonoBehaviour
+{
+
+    public GameObject TextBox;
+    public GameObject TextBox2;
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Button registerButton;
+    public Button goToLoginButton;
+    
+
+    ArrayList credentials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        registerButton.onClick.AddListener(writeStuffToFile);
+        goToLoginButton.onClick.AddListener(goToLoginScene);
+
+        if (File.Exists(Application.dataPath + "/credentials.txt"))
+        {
+            credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
+        }
+        else
+        {
+            File.WriteAllText(Application.dataPath + "/credentials.txt", "");
+        }
+
+    }
+
+    void goToLoginScene()
+    {
+        SceneManager.LoadScene("Login");
+    }
+
+
+    void writeStuffToFile()
+    {
+        bool isExists = false;
+
+        credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
+        foreach (var i in credentials)
+        {
+            if (i.ToString().Contains(usernameInput.text))
+            {
+                isExists = true;
+                break;
+            }
+        }
+
+        if (isExists)
+        {   
+            TextBox.GetComponent<Text> ().text = "" + " ";
+            Debug.Log($"Username '{usernameInput.text}' already exists");
+            TextBox2.GetComponent<Text> ().text = "" + $"Username '{usernameInput.text}' already exists";
+        }
+        else
+        {
+            TextBox2.GetComponent<Text> ().text = "" + " ";
+            credentials.Add(usernameInput.text + ":" + passwordInput.text);
+            File.WriteAllLines(Application.dataPath + "/credentials.txt", (String[])credentials.ToArray(typeof(string)));
+            Debug.Log("Account Registered");
+            TextBox.GetComponent<Text> ().text = "" + " Account Registered !!!!!!! ";
+
+        }
+    }
+
+}
