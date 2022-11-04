10524fac92341e2227e84b70057dd3cc5f987783
Thanakorn Prakobdee <thanakorn_pr@kkumail.com>
2022-11-04 20:43:38 +0700

    Beta1.3.5

--- Assets/Scripts/Play_Movement.cs
+++ Assets/Scripts/Play_Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Play_Movement : MonoBehaviour
 {
     public Chr_Control controllerScpt;
     public Animator mainAnim;
