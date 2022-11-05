f028b49894b88db0216122af5abd86cfbe07e4e6
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2022-11-05 21:42:48 +0700

    .

---
+++ Assets/Scenes/LoginandRegister/intro.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class intro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
