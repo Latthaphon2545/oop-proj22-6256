ea7af00c97065fec98a2d6b419c3c773948551a9
Thanakorn Prakobdee <thanakorn_pr@kkumail.com>
2022-11-07 19:53:28 +0700

    Beta1.4.1

--- Assets/Scripts/Heath/Heath.cs
+++ Assets/Scripts/Heath/Heath.cs
@@ -10,83 +10,159 @@ public class Heath : MonoBehaviour
     public GameObject Heath4;
     public GameObject Heath5;
 
-    int health;
+    [SerializeField] private int health;
+    Kill_Monster monster;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 2;
+        monster = GameObject.FindObjectOfType<Kill_Monster>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            health --;
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
 
-        }
+        //    health --;
 
-        if (Input.GetKey(KeyCode.E))
-        {
-            health ++;
+        //}
 
-        }
-        switch (health)
-        {
-            case 0:
-                {
-                    Heath1.gameObject.SetActive(false);
-                    Heath2.gameObject.SetActive(false);
-                    Heath3.gameObject.SetActive(false);
-                    Heath4.gameObject.SetActive(false);
-                    Heath5.gameObject.SetActive(false);
-                    break;
-                }
-            case 1:
-                {
-                    Heath1.gameObject.SetActive(true);
-                    Heath2.gameObject.SetActive(false);
-                    Heath3.gameObject.SetActive(false);
-                    Heath4.gameObject.SetActive(false);
-                    Heath5.gameObject.SetActive(false);
-                    break;
-                }
-            case 2:
-                {
-                    Heath1.gameObject.SetActive(true);
-                    Heath2.gameObject.SetActive(true);
-                    Heath3.gameObject.SetActive(false);
-                    Heath4.gameObject.SetActive(false);
-                    Heath5.gameObject.SetActive(false);
-                    break;
-                }
-            case 3:
-                {
-                    Heath1.gameObject.SetActive(true);
-                    Heath2.gameObject.SetActive(true);
-                    Heath3.gameObject.SetActive(true);
-                    Heath4.gameObject.SetActive(false);
-                    Heath5.gameObject.SetActive(false);
-                    break;
-                }
-            case 4:
-                {
-                    Heath1.gameObject.SetActive(true);
-                    Heath2.gameObject.SetActive(true);
-                    Heath3.gameObject.SetActive(true);
-                    Heath4.gameObject.SetActive(true);
-                    Heath5.gameObject.SetActive(false);
-                    break;
-                }
-            case 5:
-                {
-                    Heath1.gameObject.SetActive(true);
-                    Heath2.gameObject.SetActive(true);
-                    Heath3.gameObject.SetActive(true);
-                    Heath4.gameObject.SetActive(true);
-                    Heath5.gameObject.SetActive(true);
-                    break;
-                }
-        }
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    health ++;
+
+        //}
+
+        //if (health == 0)
+        //{
+        //    Heath1.gameObject.SetActive(false);
+        //    Heath2.gameObject.SetActive(false);
+        //    Heath3.gameObject.SetActive(false);
+        //    Heath4.gameObject.SetActive(false);
+        //    Heath5.gameObject.SetActive(false);
+        //}
+        //else if (health == 1)
+        //{
+        //    Heath1.gameObject.SetActive(true);
+        //    Heath2.gameObject.SetActive(false);
+        //    Heath3.gameObject.SetActive(false);
+        //    Heath4.gameObject.SetActive(false);
+        //    Heath5.gameObject.SetActive(false);
+        //}
+        //switch (health)
+        //{
+        //    case 0:
+        //        {
+        //            Heath1.gameObject.SetActive(false);
+        //            Heath2.gameObject.SetActive(false);
+        //            Heath3.gameObject.SetActive(false);
+        //            Heath4.gameObject.SetActive(false);
+        //            Heath5.gameObject.SetActive(false);
+        //            break;
+        //        }
+        //    case 1:
+        //        {
+        //            Heath1.gameObject.SetActive(true);
+        //            Heath2.gameObject.SetActive(false);
+        //            Heath3.gameObject.SetActive(false);
+        //            Heath4.gameObject.SetActive(false);
+        //            Heath5.gameObject.SetActive(false);
+        //            break;
+        //        }
+        //    case 2:
+        //        {
+        //            Heath1.gameObject.SetActive(true);
+        //            Heath2.gameObject.SetActive(true);
+        //            Heath3.gameObject.SetActive(false);
+        //            Heath4.gameObject.SetActive(false);
+        //            Heath5.gameObject.SetActive(false);
+        //            break;
+        //        }
+        //    case 3:
+        //        {
+        //            Heath1.gameObject.SetActive(true);
+        //            Heath2.gameObject.SetActive(true);
+        //            Heath3.gameObject.SetActive(true);
+        //            Heath4.gameObject.SetActive(false);
+        //            Heath5.gameObject.SetActive(false);
+        //            break;
+        //        }
+        //    case 4:
+        //        {
+        //            Heath1.gameObject.SetActive(true);
+        //            Heath2.gameObject.SetActive(true);
+        //            Heath3.gameObject.SetActive(true);
+        //            Heath4.gameObject.SetActive(true);
+        //            Heath5.gameObject.SetActive(false);
+        //            break;
+        //        }
+        //    case 5:
+        //        {
+        //            Heath1.gameObject.SetActive(true);
+        //            Heath2.gameObject.SetActive(true);
+        //            Heath3.gameObject.SetActive(true);
+        //            Heath4.gameObject.SetActive(true);
+        //            Heath5.gameObject.SetActive(true);
+        //            break;
+        //        }
+        //}
+
+
     }
+        public void GetHit(int damage) {
+        health = health - damage;
+        if (health == 5)
+        {
+            Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(true);
+            Heath4.gameObject.SetActive(true);
+            Heath5.gameObject.SetActive(true);
+        }
+        else if (health == 4)
+        {
+            Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(true);
+            Heath4.gameObject.SetActive(true);
+            Heath5.gameObject.SetActive(false);
+        }
+        else if (health == 3)
+        {   Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(false);
+            Heath4.gameObject.SetActive(false);
+            Heath5.gameObject.SetActive(false);
+        }
+        else if (health == 2)
+        {   Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(true);
+            Heath4.gameObject.SetActive(false);
+            Heath5.gameObject.SetActive(false);
+        }
+        else if (health == 1)
+        {
+            Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(true);
+            Heath4.gameObject.SetActive(true);
+            Heath5.gameObject.SetActive(false);
+        }
+        else
+        {
+            Heath1.gameObject.SetActive(true);
+            Heath2.gameObject.SetActive(true);
+            Heath3.gameObject.SetActive(true);
+            Heath4.gameObject.SetActive(false);
+            Heath5.gameObject.SetActive(false);
+        }
+        
+        
+    }
+
+
 }
--- Assets/Scripts/Kill_Monster.cs
+++ Assets/Scripts/Kill_Monster.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Kill_Monster : MonoBehaviour
 {
     Game_Manager gameMangerScpt;
+    Heath PlayerHealth;
+
 
     private void Start()
     {
         gameMangerScpt = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Game_Manager>();
+        PlayerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Heath>();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            gameMangerScpt.GameOver();
+            other.gameObject.GetComponent<Heath>().GetHit(1);
+            Destroy(gameObject);
         }
     }
 
